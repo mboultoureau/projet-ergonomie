e758041927b65ee6d4cc6707b8868a0230178d20
mboultoureau <contact@mboultoureau.com>
2023-11-29 10:00:19 +0100

    feat: add github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://mboultoureau.github.io',
+	base: '/projet-ergonomie',
 	integrations: [mdx(), sitemap()],
 });
